86fd09a5b8347bb8966ae216dddaa93735bb9985
F88 <F88@DESKTOP-C2NM9RC>
2018-11-20 15:27:36 +0700

    Add testcase 06

--- src/selenium_api/Topic_02_Xpath.java
+++ src/selenium_api/Topic_02_Xpath.java
@@ -1,5 +1,6 @@
 package selenium_api;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -68,18 +69,18 @@ public class Topic_02_Xpath {
 		// Step 2: Click vào link "My account" để tới trang đăng nhập
 		driver.findElement(By.xpath("//div[@class=\"footer\"]//a[@title='My Account']")).click();
 
-		//Step 3: Để trống username/password
+		// Step 3: Để trống username/password
 		driver.findElement(By.xpath("//input[@id='email']")).sendKeys();
-		driver.findElement(By.xpath("//input[@id='password']")).sendKeys();
-		
-		//Step 4: Click login button
+		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys();
+
+		// Step 4: Click login button
 		driver.findElement(By.id("send2")).click();
-		
-		//Step 5: Verify error message xuất hiện tại 2 field: This is a required field.
-		
+
+		// Step 5: Verify error message xuất hiện tại 2 field: This is a required field.
+
 		String emailErrorMessage = driver.findElement(By.id("advice-required-entry-email")).getText();
 		Assert.assertEquals(emailErrorMessage, "This is a required field.");
-		
+
 		String PasswordErrorMessage = driver.findElement(By.id("advice-required-entry-email")).getText();
 		Assert.assertEquals(PasswordErrorMessage, "This is a required field.");
 	}
@@ -101,12 +102,54 @@ public class Topic_02_Xpath {
 
 	@Test
 	public void TC_06_ReturnToSystem() {
+		
+		String firstname = "Selenium", lastname ="Online 07", email= "seleniumonline" + randomEmail() + "@gmail.com", password ="123456";
+		
+		System.out.println("Email random =" + email);
+		// Step 1: Truy cập vào trang http://live.guru99.com
+		driver.get("http://live.guru99.com");
 
+		// Step 2: Click vào link "My account" để tới trang đăng nhập
+		driver.findElement(By.xpath("//div[@class=\"footer\"]//a[@title='My Account']")).click();
+
+		// Step 3: Click vào Create an account button để tới trang đăng ký tk
+		driver.findElement(By.xpath("//span[text()='Create an Account']")).click();
+		
+		//Step 4: Nhập thông tin hợp lệ vào tất cả các field: First Name/Last Name/ Email Address/ Password/ Confirm Password
+		driver.findElement(By.id("firstname")).sendKeys(firstname);
+		driver.findElement(By.id("lastname")).sendKeys(lastname);
+		driver.findElement(By.name("email")).sendKeys(email);
+		driver.findElement(By.className("validate-password")).sendKeys(password);
+		driver.findElement(By.className("validate-cpassword")).sendKeys(password);
+		
+		//Step 5: Click REGISTER button
+		
+		driver.findElement(By.xpath("//button[@title='Register']")).click();
+		
+		//Step 6: Verify message xuất hiện khi đăng ký thành công: Thank for your registering with Main Website Store. 
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//span[text()='Thank you for registering with Main Website Store.']")).isDisplayed());
+		
+		//Step 7: Logout khỏi hệ thống 
+		driver.findElement(By.xpath("//header[@id='header']//span[text()='Account']")).click();
+		driver.findElement(By.xpath("//a[text()='Log Out']")).click();
+		
+		//Step 8: Kiểm tra hệ thống mavigate về Homepage sau khi logout thành công
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//h2[contains(text(),'This is demo site for')]")).isDisplayed());
 	}
 
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
+	}
+	
+	public int randomEmail() {
+		Random random = new Random();
+		int number = random.nextInt(999999);
+		System.out.println("Random number =" + number);
+		return number;
+		
 	}
 
 }
