3f687aec3252c13684ccdfaa6bbf039c5924f10d
Thuy <Thuy@THUY-PC>
2018-12-06 00:36:22 +0700

    TC_02_CustomDropdown

--- src/selenium_api/Topic_05_HandleDropdownList.java
+++ src/selenium_api/Topic_05_HandleDropdownList.java
@@ -1,12 +1,17 @@
 package selenium_api;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,48 +19,24 @@ import org.testng.annotations.Test;
 public class Topic_05_HandleDropdownList {
 	WebDriver driver;
 
+	WebDriverWait waitExplicit;
 	@BeforeClass
 	public void beforeClass() {
 
 		// Firefox
 		driver = new FirefoxDriver();
+		waitExplicit = new WebDriverWait(driver, 30);
+		
 		driver.get("https://www.google.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
-	@Test
-	public void TC_01_CheckTitle() throws Exception {
 
-		/*
-		 * Test Script 02: Xử lí HTML Dropdown/ list 
-		 * Step 01 - Truy cập vào trang: https://daominhdam.github.io/basic-form/index.html 
-		 * Step 02 - Kiểm tra dropdown Job Role 01 không hỗ trợ thuộc tính multi-select 
-		 * Step 03 - Chọn giá trị Automation Tester trong dropdown bằng phương thức selectVisible
-		 * Step 04 - Kiểm tra giá trị đã được chọn thành công 
-		 * Step 05 - Chọn giá trị Manual Tester trong dropdown bằng phương thức selectValue 
-		 * Step 06 - Kiểm tra giá trị đã được chọn thành công 
-		 * Step 07 - Chọn giá trị Mobile Tester trong dropdown bằng phương thức selectIndex 
-		 * Step 08 - Kiểm tra giá
-		 * trị đã được chọn thành công Step 09 - Kiểm tra dropdown có đủ 5 giá trị
-		 */
+	public void TC_01_CheckTitle() throws Exception {
 
 		driver.get("https://daominhdam.github.io/basic-form/index.html");
 		Select select = new Select(driver.findElement(By.xpath("//select[@id='job1']")));
-
-		/*
-		 * <select id="job1" name="user_job1"> 
-		 * <option value="automation">Automation Tester</option> 
-		 * <option value="manual">Manual Tester</option> 
-		 * <option value="website">Website Tester</option> 
-		 * <option value="mobile">Mobile Tester</option> 
-		 * <option value="disabled" disabled="disabled">
-		 * disable</option> 
-		 * </select>
-		 */
-
-		// Index: array 5 phần từ ( 0 1 2 3 4)
-		// Chọn
 
 		Assert.assertFalse(select.isMultiple());
 		select.selectByVisibleText("Automation Tester");
@@ -64,10 +45,61 @@ public class Topic_05_HandleDropdownList {
 		
 		select.selectByValue("manual");
 		Assert.assertEquals(select.getFirstSelectedOption().getText(), "Manual Tester");
+		Thread.sleep(3000);
 		
 		select.selectByIndex(3);
 		Assert.assertEquals(select.getFirstSelectedOption().getText(), "Mobile Tester");
+		Thread.sleep(3000);
 
+		Assert.assertEquals(select.getOptions().size(), 5);
+		
+	}
+	
+
+	@Test
+	
+	public void TC_02_CustomDropdown() throws Exception {
+		driver.get("http://jqueryui.com/resources/demos/selectmenu/default.html");
+		
+		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//li[@class='ui-menu-item']" , "19");
+		Assert.assertTrue(driver.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text' and text()='19']")).isDisplayed());
+		Thread.sleep(3000);
+		
+		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//li[@class='ui-menu-item']" , "15");
+		Assert.assertTrue(driver.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text' and text()='15']")).isDisplayed());
+		Thread.sleep(3000);
+		
+		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//li[@class='ui-menu-item']" , "5");
+		Assert.assertTrue(driver.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text' and text()='5']")).isDisplayed());
+		Thread.sleep(3000);
+		
+		driver.get("https://demos.telerik.com/kendo-ui/dropdownlist/index ");
+		
+		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//li[@class='ui-menu-item']" , "19");
+	}
+	public void selectItemInCustomDropdown(String parentXpath, String childXpath , String expectedItem) {
+		
+		//Click vao dropdown
+		WebElement element = driver.findElement(By.xpath(parentXpath));
+		element.click();
+		
+		//Get tất cả item trong dropdown vào 1 list element (List <WebElement>)
+		List<WebElement> childList = driver.findElements(By.xpath(childXpath));
+		
+		//Wait để tất cả phần tử trong dropdown được hiển thị
+		waitExplicit.until(ExpectedConditions.visibilityOfAllElements(childList));
+		
+		//Dùng vòng lặp for duyệt qua từng phần tử sau đó getText
+		for(WebElement child: childList) {
+			String textItem = child.getText();
+			System.out.println("Text in dropdown = " + textItem);
+			
+			//Nếu actual text = expected text thì click vào phần tử đó và break khỏi vòng lặp
+			if(textItem.equals(expectedItem)) {
+				child.click();
+				break;
+			}
+		}
 	}
 
 	@AfterClass
